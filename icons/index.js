851035c6f0e5188547a9016e422f0f9d8212b543
diondiondion <diondiondion@gmail.com>
2018-04-11 18:56:16 +0200

    Trying to allow skipping /icons/ path segment in imports

---
+++ icons/index.js
@@ -0,0 +1,120 @@
+const Android = require('./Android');
+const Apple = require('./Apple');
+const Appstore = require('./Appstore');
+const Arrow = require('./Arrow');
+const Asset = require('./Asset');
+const AssetAudio = require('./AssetAudio');
+const AssetDefault = require('./AssetDefault');
+const AssetDiagram = require('./AssetDiagram');
+const AssetEbook = require('./AssetEbook');
+const AssetFolder = require('./AssetFolder');
+const AssetImage = require('./AssetImage');
+const AssetPdf = require('./AssetPdf');
+const AssetPresentation = require('./AssetPresentation');
+const AssetSpreadsheet = require('./AssetSpreadsheet');
+const AssetText = require('./AssetText');
+const AssetVideo = require('./AssetVideo');
+const Box = require('./Box');
+const Chat = require('./Chat');
+const Chevron = require('./Chevron');
+const Collection = require('./Collection');
+const Download = require('./Download');
+const Dropbox = require('./Dropbox');
+const Edit = require('./Edit');
+const Ellipsis = require('./Ellipsis');
+const External = require('./External');
+const EyeClosed = require('./EyeClosed');
+const EyeOpen = require('./EyeOpen');
+const Favourite = require('./Favourite');
+const Flag = require('./Flag');
+const Fullscreen = require('./Fullscreen');
+const Hierarchy = require('./Hierarchy');
+const Home = require('./Home');
+const Info = require('./Info');
+const Link = require('./Link');
+const Logout = require('./Logout');
+const NavLeft = require('./NavLeft');
+const NavRight = require('./NavRight');
+const Ok = require('./Ok');
+const Pause = require('./Pause');
+const Pin = require('./Pin');
+const Play = require('./Play');
+const Playlist = require('./Playlist');
+const Plus = require('./Plus');
+const Search = require('./Search');
+const Send = require('./Send');
+const Settings = require('./Settings');
+const Share = require('./Share');
+const Spinnerdot = require('./Spinnerdot');
+const Star = require('./Star');
+const Tag = require('./Tag');
+const Team = require('./Team');
+const Tools = require('./Tools');
+const Trending = require('./Trending');
+const Undo = require('./Undo');
+const Unpin = require('./Unpin');
+const Upload = require('./Upload');
+const User = require('./User');
+const X = require('./X');
+
+
+module.exports = {
+	Android,
+	Apple,
+	Appstore,
+	Arrow,
+	Asset,
+	AssetAudio,
+	AssetDefault,
+	AssetDiagram,
+	AssetEbook,
+	AssetFolder,
+	AssetImage,
+	AssetPdf,
+	AssetPresentation,
+	AssetSpreadsheet,
+	AssetText,
+	AssetVideo,
+	Box,
+	Chat,
+	Chevron,
+	Collection,
+	Download,
+	Dropbox,
+	Edit,
+	Ellipsis,
+	External,
+	EyeClosed,
+	EyeOpen,
+	Favourite,
+	Flag,
+	Fullscreen,
+	Hierarchy,
+	Home,
+	Info,
+	Link,
+	Logout,
+	NavLeft,
+	NavRight,
+	Ok,
+	Pause,
+	Pin,
+	Play,
+	Playlist,
+	Plus,
+	Search,
+	Send,
+	Settings,
+	Share,
+	Spinnerdot,
+	Star,
+	Tag,
+	Team,
+	Tools,
+	Trending,
+	Undo,
+	Unpin,
+	Upload,
+	User,
+	X
+};
